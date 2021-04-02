c0a707091f11da1e20d154eb0749db5b47a0b665
Shamseen <shamseen@users.noreply.github.com>
2021-04-02 09:20:19 -0400

    setting up API call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchForm from "./components/SearchForm";
 import SearchResults from "./components/SearchResults";
 import "./styles.scss";
 import images from "./data.js";
 
 export default function App() {
+  // state vars
+  const [images, setImages] = useState([]);
+
+  // formatting GET
+  const searchOptions = {
+    key: process.env.REACT_APP_GIPHY_KEY,
+    limit: 25,
+    rating: "G",
+    api: "https://api.giphy.com/v1/gifs",
+    endpoint: "/search"
+  };
+
   return (
     <div className="App">
       <div>
